3e2bdd2f5a8f7b30d5659b9999c236e8a43aec34
ODH <awesome5-good@example.com>
2026-05-14 07:43:01 +0900

    fix: SW 즉시활성화 + 가로스크롤 완전제거

--- sw.js
+++ sw.js
@@ -9,6 +9,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
